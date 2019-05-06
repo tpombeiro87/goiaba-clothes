035406030ddbc2b40e4991a9f17f314c1d2c5ac0
tpombeiro87 <tpombeiro87@gmail.com>
2019-05-06 20:11:35 +0200

    chore: removed typeform packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,46 @@
 module.exports = {
-  extends: ['@typeform/eslint-config'],
-  rules: {
-    'react/jsx-handler-names': 'error',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-      },
-    ],
+  parser: 'babel-eslint',
+  extends: [
+    'standard',
+    'standard-jsx',
+    'plugin:react/recommended',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  plugins: ['filenames', 'jsx-a11y', 'react', 'react-perf', 'react-hooks'],
+  settings: {
+    react: {
+      version: '16',
+    },
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true,
+    },
+  },
+  rules: Object.assign({}, a11yLintRules, performanceRules, {
+    'comma-dangle': ['error', 'always-multiline'],
+    complexity: ['warn', { max: 7 }],
+    'filenames/match-regex': ['warn', '^_?[a-z0-9-.]+$'],
+    'react/jsx-sort-props': 'warn',
+    'react/jsx-handler-names': 'warn',
+    'react/jsx-no-bind': 'warn',
     'react/sort-comp': [
-      'error',
+      'warn',
       {
         order: [
           'static-methods',
+          'instance-variables',
           'lifecycle',
           '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
           'everything-else',
@@ -21,19 +49,28 @@ module.exports = {
         ],
       },
     ],
-  },
-  overrides: [
-    {
-      // remove the rule after https://github.com/Codeception/CodeceptJS/issues/1321 is solved
-      files: ['test/features/*functional-test.js'],
-      rules: {
-        'arrow-parens': ['error', 'always'],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
       },
-    },
-  ],
-  globals: {
-    Feature: true,
-    Scenario: true,
-    Helper: true,
-  },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'import/export': 'warn',
+    'prefer-template': 'warn',
+    'no-shadow': 'warn',
+    'object-shorthand': 'warn',
+    'no-else-return': 'warn',
+    'global-require': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'no-nested-ternary': 'warn',
+    'no-unneeded-ternary': 'warn',
+  })
 }
