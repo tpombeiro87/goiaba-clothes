59464226e60274bb2e134efd001b4c4cb327dbff
tpombeiro87 <tpombeiro87@gmail.com>
2019-05-07 01:23:26 +0200

    feat: removed unused now comfig / corrected stylecomponent ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
-    const page = renderPage()
-    return { ...page, pageData: 'tiago was here' }
+    const sheet = new ServerStyleSheet()
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
   }
 
   render () {
     return (
-      <html lang='pt'>
+      <html>
         <Head>
           <meta content='width=device-width, initial-scale=1' name='viewport' />
           <meta charSet='utf-8' />
-          <title>Goiaba site</title>
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
