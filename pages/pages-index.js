4a3c48c0eb12db713a1bf34ed53592eefdf38353
tpombeiro87 <tpombeiro87@gmail.com>
2019-06-05 02:41:03 +0200

    feat: improved layout and styles of pages index

--- pages/pages-index.js
+++ pages/pages-index.js
@@ -1,33 +1,39 @@
 import React from 'react'
-import Link from 'next/link'
 import styled from 'styled-components'
 
-import RegularPage from '../components/layouts/regular-page'
+import BaseLayout from '../components/layouts/base'
+import Breadcrumb from '../components/breadcrumb'
+import Link from '../components/link'
 import { pages } from '../contentful-data/utils'
 
-const InnerLink = styled.a`
-  margin-right: 20px;
-  color: black;
-  text-decoration-line: none;
-  text-transform: uppercase;
-  :hover {
-    color: #909090;
-  }
+const Root = styled.div`
+  margin-top: 2em;
+  display: flex;
+  max-width: 985px;
+  margin-bottom: 40px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
-const AllPages = () => (
-  <RegularPage>
-    <h1>Todas as Paginas</h1>
-    <ul>
-      { pages.map(page =>
-        <li key={`/${page.fields.pageId}`}>
-          <Link href={`/${page.fields.pageId}`} passHref prefetch>
-            <InnerLink>{`${page.fields.title}`}</InnerLink>
-          </Link>
-        </li>
-      )}
-    </ul>
-  </RegularPage>
-)
+const Spacer = styled.div`
+  margin-top: 15px;
+`
+
+const AllPages = () => {
+  const pageTitle = 'Todas as paginas'
+  return (
+    <BaseLayout title={pageTitle}>
+      <Root>
+        <Breadcrumb currentTitle={pageTitle} fatherLink='/' fatherTitle='Home' />
+        { pages.map(page =>
+          <Spacer key={page.fields.pageId}>
+            <Link title={page.fields.title} url={page.fields.pageId} />
+          </Spacer>
+        )}
+      </Root>
+    </BaseLayout>
+  )
+}
 
 export default AllPages
