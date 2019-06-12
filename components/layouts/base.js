417c82b8b71688c4b04ad58647023d9c75efbc6b
tpombeiro87 <tpombeiro87@gmail.com>
2019-06-12 23:38:58 +0200

    fix: double manifest

--- components/layouts/base.js
+++ components/layouts/base.js
@@ -98,7 +98,6 @@ export default class Layout extends Component {
                 : `Goiaba - site`
             }
           </title>
-          <link href='/manifest.json' rel='manifest' />
           { metaTags }
         </Head>
 
--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
           <link href='/static/favicon/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
           <link href='/static/favicon/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
           <link href='/static/favicon/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
-          <link href='/static/favicon/site.webmanifest' rel='manifest' />
+          <link href='/manifest.json' rel='manifest' />
           <link color='#5bbad5' href='/static/favicon/safari-pinned-tab.svg' rel='mask-icon' />
           <meta content='#da532c' name='msapplication-TileColor' />
           <meta content='#ffffff' name='theme-color' />
